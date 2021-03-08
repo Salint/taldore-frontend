55d4da31a1fd8ebbc5b493277746e8f572149d5e
Mohamed Abduljawad <salintofficial@hotmail.com>
2021-03-08 22:37:43 +0200

    Add project service

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,41 +1,70 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 
 import "../assets/style/pages/dashboard.css"
 import Footer from "../components/Footer";
+import ProjectService from "../services/ProjectService";
 
 class Projects extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			pending: true,
+			error: "",
+			projects: []
+		}
+	}
+
+	componentDidMount() {
+
+		(async () => {
+
+			try {
+
+				const projects = await ProjectService.listProjects();
+
+				this.setState({
+					pending: false,
+					error: "",
+					projects: projects
+				});
+
+			}
+			catch(error) {
+				this.setState({
+					pending: false,
+					error: error.message
+				})
+			}
+
+		})();
+
+	}
 
 	render() {
 
 		return (
 			<section id="dashboard">
 				<h1>Project List</h1>
+				{ this.state.pending && <h3 className="loading"><FontAwesomeIcon icon={faSyncAlt} /></h3>}
+				{ this.state.error.length > 0 && <h3 className="error">{this.state.error}</h3>}
+				{ (this.state.projects.length < 1 && !this.state.pending) && <h3 className="info">You don't have any projects yet.</h3>}
 				<section id="project-list">
-					<div className="project">
-						<h2>Project 1</h2>
-						<h3>Project description placeholder here.</h3>
-						<p>1 Member</p>
-						<a>View Project</a>
-					</div>
-					<div className="project">
-						<h2>Project 2</h2>
-						<h3>Project description placeholder here.</h3>
-						<p>3 Members</p>
-						<a>View Project</a>
-					</div>
-					<div className="project">
-						<h2>Project 2</h2>
-						<h3>Project description placeholder here.</h3>
-						<p>2 Members</p>
-						<a>View Project</a>
-					</div>
-					<div className="project">
-						<h2>Project 2</h2>
-						<h3>Project description placeholder here.</h3>
-						<p>2 Members</p>
-						<a>View Project</a>
-					</div>
+
+					{
+						this.state.projects.map(project =>
+							<div className="project">
+								<h2>{project.name}</h2>
+								<h3>{project.description}</h3>
+								<p>{project.memberCount} Member</p>
+								<a href={"/project/" + project.id}>View Project</a>
+							</div>)
+					}
+
 				</section>
 			</section>
 		)
---
+++ src/services/ProjectService.js
@@ -0,0 +1,36 @@
+import firebase from "./FirebaseService";
+
+class ProjectService {
+
+	static async listProjects() {
+
+		let projects = [];
+
+		try {
+
+			const results = await firebase.firestore().collection("posts").where("members", "array-contains", firebase.auth().currentUser.uid).get();
+			const docs = results.docs;
+
+			for(let i = 0; i < docs.length; i++) {
+				projects.push({
+					id: docs[i].id,
+					name: docs[i].get("name"),
+					description: docs[i].get("description"),
+					memberCount: docs[i].get("members").length
+				})
+			}
+
+		}
+		catch(error) {
+
+			throw error;
+		}
+		finally {
+			return projects;
+		}
+
+	}
+
+}
+
+export default ProjectService;
