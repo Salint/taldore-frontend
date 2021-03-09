aab2a1535fcb368c082e251c023ad20ea09598a1
Mohamed Abduljawad <salintofficial@hotmail.com>
2021-03-09 20:03:53 +0200

    Add creating project to Project Service

--- src/services/ProjectService.js
+++ src/services/ProjectService.js
@@ -30,6 +30,39 @@ class ProjectService {
 		}
 
 	}
+	static async createProject(name, description) {
+
+		let id;
+
+		if(name.length < 1 || description.length < 1 ) {
+			throw new Error("Some fields are empty");
+		}
+		else {
+
+			try {
+				const results = await firebase.firestore().collection("projects").add({
+					name: name,
+					description: description,
+					created: new Date(),
+					owner: firebase.auth().currentUser.uid,
+					members: [
+						firebase.auth().currentUser.uid
+					]
+				});
+
+				id = results.id;
+			}
+			catch(error) {
+
+				throw error;
+			}
+
+			finally {
+				return id;
+			}
+		}
+
+	}
 
 }
 
